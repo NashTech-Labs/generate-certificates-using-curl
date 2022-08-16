4cf9b034b62e8f2c09163093d074bd0160d2d1b9
Nitin Mishra <76727343+imnitin28@users.noreply.github.com>
2022-08-16 12:06:36 +0530

    Delete retrieve_decoded_certificate.py

--- retrieve_decoded_certificate.py
+++
@@ -1,4 +0,0 @@
-import base64
-with open('encoded_values', 'r') as encoded, open('decoded_values', 'wb') as decoded:
-    for line in encoded.readlines():
-        decoded.write(base64.b64decode(line) + b'\n')
